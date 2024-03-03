c4ec7f4e1b50b15b9f70495e81585d0893d99d53
aboodabdo347 <aboodabdo347@gmail.com>
2024-03-03 09:16:27 +0300

    starting in stadiums

--- vite-project/src/pages/StadiumsList.jsx
+++ vite-project/src/pages/StadiumsList.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useRef, useState } from 'react'
+import Client from '../services/api'
+import Search from '../components/Search'
+import StadiumCard from '../components/StadiumCard'
 const StadiumsList = () => {
+  const searchRef = useRef(null)
+  const [searchResults, setSearchResults] = useState([])
+  const [stadiums, setStadiums] = useState([])
+  const [pressed, setPresssed] = useState(false)
+  useEffect(() => {
+    Client.get('/stadiums')
+      .then((response) => {
+        setStadiums(response.data)
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const searchTerm = searchRef.current.value.toLowerCase()
+    const filteredTeams = stadiums.filter((stadium) =>
+    stadium.name.toLowerCase().includes(searchTerm)
+    )
+    setSearchResults(filteredTeams)
+    setPresssed(true)
+  }
+
   return (
     <div>
       <h1>StadiumsList</h1>
